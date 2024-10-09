e89f249cba4f9b2f88558c574920b66764379eb7
HenrySantoso <119570803+HenrySantoso@users.noreply.github.com>
2024-10-09 16:30:00 +0700

    coba komen

--- obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157f22189d3a419bd29c146c8ec8485764003487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4718ea9021ebca3a465b6d72e4cf60cc6d789e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
